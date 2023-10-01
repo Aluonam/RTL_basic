b167e612f9ead63eb147f1e60492708568082c55
Paula Cano <paulacanoroman@gmail.com>
2023-10-01 20:56:08 +0200

    nuevo proyecto test inicial de input y botón
    npm run test

---
+++ prueba-testing-01/src/UserForm.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react'
+import user from '@testing-library/user-event';
+import userForm from './UserForm';
+import UserForm from './UserForm';
+
+test('muestra dos entradas y un botón', ()=>{
+    // Renderizar el componente
+    render(<UserForm/>);
+
+    // Manipular el componente o elementos en él
+    const inputs = screen.getAllByRole('textbox');
+    const button = screen.getByRole('button');
+
+    // Assertion: lo que el compoennte debe hacer
+    // Lo esperado que haga 
+    expect(inputs).toHaveLength(2);
+    expect(button).toBeInTheDocument();
+
+    
+});
