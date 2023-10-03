30a873ebb18188a9b0e317e3bb2a18e9924111cd
Paula Cano <paulacanoroman@gmail.com>
2023-10-03 15:32:58 +0200

    renderiza el email y nombre de cada usuario

--- prueba-testing-01/src/UserList.test.js
+++ prueba-testing-01/src/UserList.test.js
@@ -1,5 +1,4 @@
-import { render, screen } from '@testing-library/react';
-import user from './UserList';
+import { render, screen, within } from '@testing-library/react';
 import UserList from './UserList';
 
 
@@ -10,16 +9,29 @@ test('muestra una fila por usuario', ()=>{
         { name: 'jane', email: 'jane@jane.com' },
         { name: 'bob', email: 'bob@bob.com' }
     ];
-    render (<UserList users={users} />)
+    // render (<UserList users={users} />)
+    const { container } = render(<UserList users={users}/>)
 
     //Encontrar las columnas en la tabla
     // screen.logTestingPlaygroundURL();
-    const rows = screen.getAllByRole('row')
+    const rows = within(screen.getByTestId('users')).getAllByRole('row'); //within se importa y usar para que encuentre los elementos dle cuerpo
 
     //Assertion: numero correcto de filas en la tabla
-    expect(rows).toHaveLength(3);
+    expect(rows).toHaveLength(2); //una fila es el encabezado
 });
 
 test('rendriza el email y nombre de cada usuario', ()=>{
+    const users = [
+        { name: 'jane', email: 'jane@jane.com' },
+        { name: 'bob', email: 'bob@bob.com' }
+    ];
+    const { container } = render(<UserList users={users}/>)
 
+    for (let user of users){
+        const name = screen.getByRole('cell', { name: user.name });
+        const email = screen.getByRole('cell', { name: user.email });
+
+        expect(name).toBeInTheDocument();
+        expect(email).toBeInTheDocument();
+    }
 });
