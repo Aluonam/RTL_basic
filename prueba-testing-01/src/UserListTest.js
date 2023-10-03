995a2cda8c9cb42be0755ee37f2fd91050a5c841
Paula Cano <paulacanoroman@gmail.com>
2023-10-03 11:26:14 +0200

    Estructura test renderizar filas

---
+++ prueba-testing-01/src/UserListTest.js
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import user from './UserList';
+
+
+test('muestra una fila por usuario', ()=>{
+    //Renderizar componente
+
+    //Encontrar las columnas en la tabla
+
+    //Assertion: numero correcto de filas en la tabla
+
+});
+
