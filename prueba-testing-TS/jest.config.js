6983e95fe2fde8b227f6b04621d8ce6a2b62284b
Aluon <paulacanoroman@gmail.com>
2024-08-22 13:41:30 +0200

    fix: err styles path

--- prueba-testing-TS/jest.config.js
+++ prueba-testing-TS/jest.config.js
@@ -1,21 +1,23 @@
-const nextJest = require('next/jest')
+const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
   dir: './',
-})
+});
 
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
-    // Handle module aliases (this will be auto-generated in next.config.js)
+    '^@/(.*)$': '<rootDir>/$1', // Asegúrate de que este alias está bien definido
     '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/styles/(.*)$': '<rootDir>/styles/$1',
+    '^@/pages/(.*)$': '<rootDir>/pages/$1',
   },
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
   collectCoverageFrom: [
     'components/**/*.{ts,tsx}',
     '!components/**/*.d.ts',
   ],
-}
+};
 
-module.exports = createJestConfig(customJestConfig)
+module.exports = createJestConfig(customJestConfig);
--- prueba-testing-TS/pages/index.tsx
+++ prueba-testing-TS/pages/index.tsx
@@ -1,9 +1,8 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import ComponenteDummy from '../components/ComponenteDummy/ComponenteDummy'
+import Counter from '../components/Counter/Counter'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -14,9 +13,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <main className={`${styles.main}`}>
         
-        
+        <ComponenteDummy></ComponenteDummy>
+        <Counter></Counter>
       </main>
     </>
   )
