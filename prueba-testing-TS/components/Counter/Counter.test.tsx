b3257a5a052d77e76c40fff9a5fe6b043fd358b6
Aluon <paulacanoroman@gmail.com>
2024-08-22 14:04:25 +0200

    feat: test counter with fireEvent

--- prueba-testing-TS/components/Counter/Counter.test.tsx
+++ prueba-testing-TS/components/Counter/Counter.test.tsx
@@ -1,7 +1,27 @@
 import '@testing-library/jest-dom';
 import Counter from "./Counter";
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 test('render counter component', ()=>{
     render(<Counter></Counter>)
 })
+
+// EJEMPLO CON FIREEVENT
+test('render component and increment value', async ()=>{
+    render(<Counter></Counter>)
+    
+    // Encuentra el botón
+    const buttonIncrement = screen.getByRole('button', {name: '+'});
+    
+    // Verifica que incialmente es 0
+    expect(screen.getByText('0')).toBeInTheDocument()
+
+    // Haz click para sumar 
+    fireEvent.click(buttonIncrement);
+
+    // Verifica que se ha sumado
+    expect(screen.getByText('1')).toBeInTheDocument()
+})
+
+
